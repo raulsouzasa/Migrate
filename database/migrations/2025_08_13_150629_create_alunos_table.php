971d44e02e332c975e19aa48d9b05bd5fa75d19b
raulsouzasa <raul.sa@etec.sp.gov.br>
2025-08-13 12:19:33 -0300

    Migration

---
+++ database/migrations/2025_08_13_150629_create_alunos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone')->nullable();
+            $table->date('data_nascimento');
+            $table->string('curso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alunos');
+    }
+};
